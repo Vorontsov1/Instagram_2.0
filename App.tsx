2aa9339f1551088e941e909f0f5ab699a2df5652
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-07-23 11:02:37 +0300

    4.1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen/HomeScreen';
-import CommentsScreen from './src/screens/CommentScreen';
-import ProfileScreen from './src/screens/ProfileScreen/ProfileScreen';
-import EditProfielScreen from './src/screens/EditProfileScreen/EditProfileScreen';
-import PostUploadScreen from './src/screens/PostUploadScreen';
+import {StyleSheet, SafeAreaView} from 'react-native';
+// import HomeScreen from './src/screens/HomeScreen/HomeScreen';
+//  import CommentScreen from './src/screens/CommentScreen';
+// import ProfileScreen from './src/screens/ProfileScreen/ProfileScreen';
+// import EditProfielScreen from './src/screens/EditProfileScreen/EditProfileScreen';
+// import PostUploadScreen from './src/screens/PostUploadScreen';
 
+import Navigation from './src/navigation';
 
 const App = () => {
   return (
-    <SafeAreaView style={ styles.app }>
-      {/* <HomeScreen /> */}
-      {/* <CommentsScreen /> */ }
-      {/* <ProfileScreen /> */ }
-      {/* <EditProfielScreen /> */ }
-      <PostUploadScreen />
+    <SafeAreaView style={styles.app}>
+     <Navigation />
     </SafeAreaView>
   );
 }
